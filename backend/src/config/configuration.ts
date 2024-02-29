9f1c4434cf1d17727f1142fd94e4602c3a981423
Evgeniia Zubova <evgeniia.zubova@BR-C02ZNM1XMD6P.local>
2024-02-29 16:15:45 +0400

    chore: add docker config

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -3,14 +3,14 @@ export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
   },
   db: {
-    host: process.env.PG_HOST,
+    host: process.env.POSTGRES_HOST,
     port: parseInt(process.env.PG_PORT, 10) || 5432,
-    name: process.env.PG_DB,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    name: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
   },
   jwt: {
-    jwt_secret: process.env.SECRET,
+    jwt_secret: process.env.JWT_SECRET,
     expiresIn: process.env.EXPIRES_IN || '12h',
   },
 });
